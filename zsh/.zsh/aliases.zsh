32f474b54018b71c126202376f0873de40aa3c5f
rchrd <26688060+rchrd-0@users.noreply.github.com>
2026-06-22 17:14:04 +0800

    update: 260622

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -8,7 +8,8 @@ alias sync-secrets="$HOME/.dotfiles/scripts/sync-secrets.sh"
 alias ytdlp="yt-dlp --cookies-from-browser safari"
 
 alias cxt="autossh -M 0 -N -D 1080 codex-exit"
-alias cx='ALL_PROXY=http://127.0.0.1:3128 HTTPS_PROXY=http://127.0.0.1:3128 HTTP_PROXY=http://127.0.0.1:3128 NO_PROXY=127.0.0.1,localhost,::1 no_proxy=127.0.0.1,localhost,::1 codex'
+# alias cx='ALL_PROXY=http://127.0.0.1:3128 HTTPS_PROXY=http://127.0.0.1:3128 HTTP_PROXY=http://127.0.0.1:3128 NO_PROXY=127.0.0.1,localhost,::1 no_proxy=127.0.0.1,localhost,::1 codex'
+alias cx="pe codex"
 
 # alias lvim='NVIM_APPNAME=lazyvim \nvim'
 
